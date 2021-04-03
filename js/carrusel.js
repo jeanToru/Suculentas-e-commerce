5d183181f98ebe2978fc86e5114d42838598b329
Jean Brown <jeantoruno00@gmail.com>
2021-04-02 17:12:29 -0700

    Carrusel Novo

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,34 +1,19 @@
-const carrusel = document.querySelector('#carrusel');
-const carruselSection = document.querySelectorAll('.carrusel--section');
-const carruselSectionLast = carruselSection[carruselSection.length - 1];
-const btnBack = document.querySelector('.arrowBack');
-const btnGo = document.querySelector('.arrowGo');
+const buttonsWrapper = document.querySelector('.map');
+const slides = document.querySelector('.inner');
 
-carrusel.insertAdjacentElement('afterbegin', carruselSectionLast);
-
-function moveright() {
-  const carruselSectionFirst = document.querySelector('.carrusel--section');
-  carrusel.style.marginLeft = '-200%';
-  carrusel.style.transition = 'all 0.5s';
-  setTimeout(() => {
-    carrusel.style.transition = 'none';
-    carrusel.insertAdjacentElement('beforeend', carruselSectionFirst);
-    carrusel.style.marginLeft = '-100%';
-  }, 500);
-}
-
-function moveleft() {
-  const carruselSection1 = document.querySelector('.carrusel--section');
-  const carruselSectionLast1 = carruselSection1[carruselSection1.length - 1];
-  carrusel.style.marginLeft = '0%';
-  carrusel.style.transition = 'all 0.5s';
-  setTimeout(() => {
-    carrusel.style.transition = 'none';
-    carrusel.insertAdjacentElement('afterbegin', carruselSectionLast1);
-    carrusel.style.marginLeft = '-100%';
-  }, 500);
-}
-
-btnGo.addEventListener('click', moveright);
-
-btnBack.addEventListener('click', moveleft);
+buttonsWrapper.addEventListener('click', e => {
+  if (e.target.nodeName === 'BUTTON') {
+    Array.from(buttonsWrapper.children).forEach(item =>
+      item.classList.remove('active'));
+    if (e.target.classList.contains('first')) {
+      slides.style.transform = 'translateX(-0%)';
+      e.target.classList.add('active');
+    } else if (e.target.classList.contains('second')) {
+      slides.style.transform = 'translateX(-33.33333333333333%)';
+      e.target.classList.add('active');
+    } else if (e.target.classList.contains('third')) {
+      slides.style.transform = 'translatex(-66.6666666667%)';
+      e.target.classList.add('active');
+    }
+  }
+});
