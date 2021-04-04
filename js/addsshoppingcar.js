9fa262ac123e5987c0fe88e904a56b809f088735
José Daniel Ramos Barquero <josedanielramosbarquero@gmail.com>
2021-04-04 17:22:23 -0600

    agrego cambios a las paginas de producto

--- js/addsshoppingcar.js
+++ js/addsshoppingcar.js
@@ -1,11 +1,10 @@
 const adds = document.getElementById('addjs');
+const addsmovil = document.getElementById('addjs-movil');
 const info = document.querySelector('.list-container');
 const nombre = document.querySelector('h2');
 const img = document.querySelectorAll('img')[9];
 const precio = document.querySelectorAll('p')[6];
 let ident = img.getAttribute('src');
-
-
 const open = document.getElementById('open');
 const alertContainer = document.getElementById('alert__Container');
 
@@ -21,6 +20,37 @@ function pushinfo(infopush) {
 }
 
 adds.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (ident.length === 9) {
+    ident = ident.substring(4, 5);
+  } else if (ident.length === 10) {
+    ident = ident.substring(4, 6);
+  }
+
+  const infopush = {
+    'img': ident,
+    'name': nombre.textContent,
+    'priceProduct': precio.textContent
+  }
+  pushinfo(infopush);
+
+  alertContainer.innerHTML = `
+    <div class="alert alert--product">
+      <img src="img/check.png" alt="check">
+      <h2>Se agrego su producto</h2>
+      <button class=" button-principal details" id="close">ok</button>
+    </div>`;
+  alertContainer.classList.add('show');
+  const close = document.getElementById('close');
+
+  close.addEventListener('click', () => {
+    alertContainer.classList.remove('show');
+  });
+});
+
+// Movil
+addsmovil.addEventListener('click', (event) => {
   event.preventDefault();
 
   if (ident.length === 9) {
